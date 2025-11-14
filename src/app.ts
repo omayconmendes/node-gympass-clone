c53fcba002eee4213fadc8fa3e4afa8a52163f69
Maycon Mendes <mayconsouzamendes@gmail.com>
2025-11-13 22:16:53 -0300

    feat: Instala prisma e configura docker compose. Cria models e migrations.

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,13 @@
 import fastify from "fastify"
+import { PrismaClient } from "@prisma/client"
+
 export const app = fastify();
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+    data: {
+        name: 123,
+        email: 'mayconsouzamendes@gmail.com'
+    }
+});
